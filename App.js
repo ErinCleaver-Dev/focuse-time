5a43276c0afb07025b409a99aad4eb13e4f3ecb1
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-04-04 17:13:31 -0400

    0.0.10
    Added Const for focus subject.

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [focusSubject, setFocusSubject] = useState(null);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{
+      focusSubject ? 
+      <Text> Here is where the timer will be</Text> : 
+      <Text>
+        Bulding input for subject
+      </Text>
+      }
+      </Text>
     </View>
   );
 }
@@ -14,8 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
   },
 });
